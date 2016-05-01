a375061e7c2a2373ca8dc8ef903c39f5469d7ca9
nabuchodonozor <b.piwnik@o2.pl>
2016-05-01 15:09:12 +0200

    Initial commit

---
+++ src/main/java/RegisterView.java
@@ -0,0 +1,46 @@
+/**
+ * Created by Bartek on 2016-04-30.
+ */
+
+import javax.faces.bean.ManagedBean;
+
+@ManagedBean
+public class RegisterView {
+
+    private String firstname;
+    private String lastname;
+    private String password;
+    private String password2;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword2() {
+        return password2;
+    }
+
+    public void setPassword2(String password2) {
+        this.password2 = password2;
+    }
+}
